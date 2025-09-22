7baf22baef2df8c34f73cf5a426abe66fa3d8b5b
luvelio <145437564+luvelio@users.noreply.github.com>
2025-09-22 13:39:07 +0800

    Add files via upload

---
+++ views/dashboard/components/mixins/resize.js
@@ -0,0 +1,56 @@
+import { debounce } from '@/utils'
+
+export default {
+  data () {
+    return {
+      $_sidebarElm: null
+    }
+  },
+  mounted () {
+    this.$_initResizeEvent()
+    this.$_initSidebarResizeEvent()
+  },
+  beforeDestroy () {
+    this.$_destroyResizeEvent()
+    this.$_destroySidebarResizeEvent()
+  },
+  // to fixed bug when cached by keep-alive
+  activated () {
+    this.$_initResizeEvent()
+    this.$_initSidebarResizeEvent()
+  },
+  deactivated () {
+    this.$_destroyResizeEvent()
+    this.$_destroySidebarResizeEvent()
+  },
+  methods: {
+    $_resizeHandler () {
+      return debounce(() => {
+        if (this.echartsUserAction) {
+          this.echartsUserAction.resize()
+        }
+        if (this.echartsQuestion) {
+          this.echartsQuestion.resize()
+        }
+      }, 100)()
+    },
+    $_initResizeEvent () {
+      window.addEventListener('resize', this.$_resizeHandler)
+    },
+    $_destroyResizeEvent () {
+      window.removeEventListener('resize', this.$_resizeHandler)
+    },
+    $_sidebarResizeHandler (e) {
+      if (e.propertyName === 'width') {
+        this.$_resizeHandler()
+      }
+    },
+    $_initSidebarResizeEvent () {
+      this.$_sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+      this.$_sidebarElm && this.$_sidebarElm.addEventListener('transitionend', this.$_sidebarResizeHandler)
+    },
+    $_destroySidebarResizeEvent () {
+      this.$_sidebarElm && this.$_sidebarElm.removeEventListener('transitionend', this.$_sidebarResizeHandler)
+    }
+  }
+}
